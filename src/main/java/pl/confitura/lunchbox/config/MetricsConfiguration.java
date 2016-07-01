666e4afbde3a4fb2e41bccd4f43a0901b9a4950a
Adam Dubiel <adam.dubiel@allegrogroup.com>
2016-07-02 00:41:57 +0200

    jvm metrics

--- src/main/java/pl/confitura/lunchbox/config/MetricsConfiguration.java
+++ src/main/java/pl/confitura/lunchbox/config/MetricsConfiguration.java
@@ -1,8 +1,12 @@
 package pl.confitura.lunchbox.config;
 
+import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.MetricSet;
 import com.codahale.metrics.graphite.Graphite;
 import com.codahale.metrics.graphite.GraphiteReporter;
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
 import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 import javax.annotation.PostConstruct;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -30,5 +35,21 @@ public class MetricsConfiguration {
                 .prefixedWith("services.lunchbox." + hostname)
                 .build(graphite);
         reporter.start(10, TimeUnit.SECONDS);
+    }
+
+    @PostConstruct
+    public void registerJvmMetrics() {
+        registerAll("gc", new GarbageCollectorMetricSet(), metricRegistry);
+        registerAll("memory", new MemoryUsageGaugeSet(), metricRegistry);
+    }
+
+    private void registerAll(String prefix, MetricSet metricSet, MetricRegistry registry) {
+        for (Map.Entry<String, Metric> entry : metricSet.getMetrics().entrySet()) {
+            if (entry.getValue() instanceof MetricSet) {
+                registerAll(prefix + "." + entry.getKey(), (MetricSet) entry.getValue(), registry);
+            } else {
+                registry.register(prefix + "." + entry.getKey(), entry.getValue());
+            }
+        }
     }
 }
